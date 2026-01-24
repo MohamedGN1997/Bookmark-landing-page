61203f511a3e63dbf70bfc3d446b627997f0efa0
Mohamed Guenfoudi <mohamed.gn1997@outlook.com>
2026-01-24 20:53:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,41 @@ humbrgerIconOpen.addEventListener("click",function(){
     console.log("Hola Cruel open")
 });
 
+
+
+// Start features-section "Slider"
+let orderes = document.querySelectorAll("body main section.section_0 .container .features-section .orders .order");
+let contentsSlider = document.querySelectorAll("body main section.section_0 .container .features-section .slider .contentsSlider");
+for(i=0;i<orderes.length;i++){
+    orderes[i].addEventListener("click",function(){
+        this.classList.add("active");
+        console.log(this)
+        for(let x = 0;x<orderes.length;x++){
+            if(orderes[x] !== this){
+                orderes[x].classList.remove("active");
+                contentsSlider[x].classList.remove("active");
+            }else{
+                contentsSlider[x].classList.add("active");
+            }
+        }
+    })
+}
+
+
+// Start faq-item  "show the text of questions"
+let faqItems = document.querySelectorAll("body main section.section_2 .container  .faq-list .faq-item");
+let contentText = document.querySelectorAll("body main section.section_2 .container  .faq-list .faq-item .content-text");
+faqItems.forEach(function(faqItem){
+    faqItem.addEventListener("click",function(){
+        faqItem.children[0].children[1].children[1].classList.add("active");
+        faqItem.children[0].children[1].children[0].classList.remove("active");
+        faqItem.children[1].classList.add("active");
+        for(let i = 0;i<faqItems.length;i++){
+            if(faqItems[i] !== faqItem){
+                faqItems[i].children[0].children[1].children[1].classList.remove("active");
+                faqItems[i].children[0].children[1].children[0].classList.add("active");
+                faqItems[i].children[1].classList.remove("active");
+            }
+        }
+    });
+})
